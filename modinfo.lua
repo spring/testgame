2e71f8eff40ab47b867cf90afcaa8d13471da4a1
abma <spring@abma.de>
2013-07-25 18:46:23 +0200

    rename

--- modinfo.lua
+++ modinfo.lua
@@ -1,6 +1,6 @@
 local modinfo = {
-	name = "Spring engine test game",
-	shortName "testgame",
+	name = "test game",
+	shortName = "testgame",
 	version = "$VERSION",
 	description =	"Test game for testing the lua interface and the sim of spring",
 	url =	"http://github.com/spring/testgame",
